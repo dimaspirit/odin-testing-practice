7ce1853746441e877a83f32ce0712806e7ad99d7
Dmytro Lobanov <dspwww@gmail.com>
2025-05-04 14:30:18 +0100

    feat: Add reverseString & validateString

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,23 @@
-export function capitalize(string) {
-  if(typeof string !== 'string') throw new Error('The value should be a string');
-  if(string.trim() === '') throw new Error('The value should not be empty');
-  return `${string[0].toUpperCase()}${string.substring(1)}`;
+function isString(value) {
+  return (typeof value === 'string') ? true : false;
+}
+
+function isStringEmpty(string) {
+  return (string.trim() === '') ? true : false;
+}
+
+function validateString(value) {
+  if(!isString(value)) throw new Error('The value should be a string');
+  if(isStringEmpty(value)) throw new Error('The value should not be empty');
+}
+
+export function capitalize(value) {
+  validateString(value);
+  return `${value[0].toUpperCase()}${value.substring(1)}`;
+}
+
+export function reverseString(value) {
+  if(!isString(value)) throw new Error('The value should be a string');
+  if(isStringEmpty(value)) throw new Error('The value should not be empty');
+  return [...value].reverse().join("");
 }
--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,15 +1,29 @@
-import { capitalize } from "./helpers";
+import { capitalize, reverseString } from "./helpers";
 
 describe('Capitalize ', () => {
   test('should work correct', () => {
     expect(capitalize('welcome')).toBe('Welcome');
   });
-  
+
   test('return error when value has invalid type', () => {
     expect(() => capitalize(true)).toThrow(/string/);
   });
-  
+
   test('return error when value is empty', () => {
     expect(() => capitalize('   ')).toThrow(/empty/);
+  });
+})
+
+describe('reverseString', () => {
+  test('should work correct', () => {
+    expect(reverseString('welcome')).toBe('emoclew');
+  });
+  
+  test('return error when value has invalid type', () => {
+    expect(() => reverseString(null)).toThrow(/string/);
+  });
+  
+  test('return error when value is empty', () => {
+    expect(() => reverseString('   ')).toThrow(/empty/);
   });
 })
